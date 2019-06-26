4b54d4d9499d99fe6f1ebf8da22c2139f9078cd2
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2019-06-26 14:06:42 -0400

    V0.0
    Un personnage qui bouge, vite fait une map, un dialogue non attribué, du fun :D

---
+++ Rpg De La Moerte/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody rb;
+    private string movementAxeHorizontal;
+    private string movementAxeVertical;
+
+    // Start is called before the first frame update
+     void Start()
+    {
+        movementAxeHorizontal = "Horizontal";
+        movementAxeVertical = "Vertical";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movementHorizontal = Input.GetAxis(movementAxeHorizontal);
+        float movementVertical = Input.GetAxis(movementAxeVertical);
+        Vector3 movement = new Vector3(movementHorizontal,movementVertical,0.0f);
+
+        rb.MovePosition(transform.position + movement*speed*Time.deltaTime);
+    }
+
+}
